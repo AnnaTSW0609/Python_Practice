c518eea92c9326b2f25e7707055158a54d075d39
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-02 08:03:26 +0800

    Create Rosalind_Practice_3.py

---
+++ Rosalind_Practice_3.py
@@ -0,0 +1,19 @@
+DNA_string = "AAAACCCGGT"
+
+complimentary = ""
+
+for letter in DNA_string: 
+
+    if letter == "A":
+        complimentary += "T"
+
+    elif letter == "T":
+        complimentary += "A"
+
+    elif letter == "G":
+        complimentary += "C"
+
+    else:
+        complimentary += "G"
+
+print(complimentary [::-1])
