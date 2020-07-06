d8f05eac408b894683810b091fbf9addd92e6216
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-06 16:13:15 +0800

    Create Rosalind_Practice.py

---
+++ Rosalind_Practice.py
@@ -0,0 +1,51 @@
+"""Reading sequences from fasta file and find the sequence with the highest GC%"""
+
+# used "awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < rosalind_gc.txt > new_rosalind_gc.txt"
+
+# in Bash to solve the problem (cd Desktop first, and need to delete the first empty line)
+
+# because the fasta file is not in the same line 
+
+with open("/Users/annatswater/Desktop/new_rosalind_gc.txt","r+") as f:
+
+    # all_seq = {} # empty dictionary to store the sequences
+
+    name = []
+
+    seq = []
+
+    for line in f: # save names and sequences separately 
+
+        line = line.rstrip()
+
+        if ">" in line:
+
+            line = line.replace(">","")
+            
+            name.append(line)
+
+        else:
+
+            count = 0
+
+            for char in line:
+
+                if char == "G" or char == "C":
+
+                    count += 1
+
+            GC_count = count/len(line)
+
+            seq.append(GC_count)
+    
+
+    max_item = max(seq)
+
+    print(name[seq.index(max_item)])
+
+    print("{:.6f}".format(max_item*100)) # you must get the % or else it wouldn't be right! times 100! 
+    
+
+            
+
+    
