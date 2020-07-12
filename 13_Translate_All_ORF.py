61720189aad3f386ea63b2cf33780588b3b862d6
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-12 19:03:45 +0800

    Update 13_Translate_All_ORF.py

--- 13_Translate_All_ORF.py
+++ 13_Translate_All_ORF.py
@@ -1,3 +1,55 @@
+"""Finding motif, from 11"""
+
+# Reference:
+# http://recologia.com.br/2013/06/problema-rosalind-open-reading-frames-orf/
+
+DNA = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
+
+motif = "ATG"
+
+def ReverseComplement(string): # need to consider reverse complementary string and its ORF as well
+
+    intab = "ATCG"
+    outtab = "TAGC" # corresponding position for elements to be replaced
+    trantab = string.maketrans(intab, outtab) # provide template
+    return(string.translate(trantab) [::-1]) # translate the string in the pattern defined in trantab, + reverse in our case
+
+Rev_DNA = ReverseComplement(DNA)
+
+
+# Reference:
+# https://www.geeksforgeeks.org/python-count-overlapping-substring-in-a-given-string/
+
+
+def CountOccurrences(string, substring): 
+  
+    # Initialize start to 0 and list for all position to empty 
+    start = 0
+    all_pos = ""
+
+    # Search through the string till 
+    # we reach the end of it 
+    while start < len(string): # forever loop back until I break it
+        
+        pos = string.find(substring, start) # default start = 0
+        
+        if pos != -1: 
+            start = pos + 1 # loop back to search from after first match after while, +1 becasue answer wants 1-based index
+            all_pos += str(pos) # so use original for 0-based index, since we are dealing with python strings 
+            all_pos += " "
+        
+        else: 
+            # If no further substring is present, pos will = -1 
+            break
+
+    
+    return(all_pos.split())
+
+ATG_pos = CountOccurrences(DNA, motif)
+
+Rev_ATG_pos = CountOccurrences(Rev_DNA, motif)
+
+
 """Translation from ALl ORF in a sequence, modification of 8"""
 
 # Generate codon table as a python dictionary 
@@ -8,30 +60,47 @@ codons = [a + b + c for a in bases for b in bases for c in bases] # random formi
 amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'# already in the right order 
 codon_table = dict(zip(codons, amino_acids))
 
-print(codon_table)
-
-def translate (seq):
+def translate (ATG_positions, seq): # seq = a string to be translated, ATG_positions = a list of positions of ORF
     
     peptide = ""
     
-    all_possible_seq = []
+    all_possible_pep = [] # for storing all possible translation
     
-    for i in range(0, len(seq), 3): # range(start, stop, step) https://www.geeksforgeeks.org/python-range-function/
-        
-        codon = seq [i: i+3] # +3 so that cover the third base, index issue
-        
-        amino_acid = codon_table.get(codon, '*')
+    for item in ATG_positions: # starting from the ORFs
+        item = int(item)
+        for i in range(item, len(seq), 3):
+            codon = seq [i: i+3] # +3 so that cover the third base, index issue
+            amino_acid = codon_table.get(codon, '*')
 
-        if amino_acid == "ATG":
+            if amino_acid != '*': # use dictionary get method to obtain the amino acid, which is the value for the key (codon)
+                peptide += amino_acid
             
-            peptide += amino_acid
-           
-        elif amino_acid != '*': # use dictionary get method to obtain the amino acid, which is the value for the key (codon)
-            
-            peptide += amino_acid
-            
-        else: # * =  stop codon, so if have * break
-            all_possible_seq.append(peptide)
-            peptide = ""
+            else:
+                peptide += amino_acid # so I'll include the * in the sequence to distinguish
+                # as only peps with start and stop without stop in between are ORFs
+                break
+                
+        all_possible_pep.append(peptide) # this must be inside the loop, so it would append all the items with the opening index in ATG_position
+        peptide = ""
 
-translate("AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG")
+    new_possible_pep = [] # for storing all with start and *
+    
+    for item in all_possible_pep:
+        if "*" in item:
+            new_possible_pep.append(item.replace("*", "")) # if replace in original string will cause confusion
+            
+    
+    return(new_possible_pep) # if print, the result would be a non-type instead if you want to save it as below of a variable
+    
+    
+F = translate(ATG_pos, DNA)
+
+R= translate(Rev_ATG_pos, Rev_DNA)
+
+for item in list(set(F+R)):
+    print(item) # intersection between the two lists
+        
+        
+    
+    
+    
