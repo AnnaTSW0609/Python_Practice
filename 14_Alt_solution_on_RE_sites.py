d422ff79876c6972f755ddc37bcfb74a04daabb5
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-14 11:24:39 +0800

    Update 14_Alt_solution_on_RE_sites.py

--- 14_Alt_solution_on_RE_sites.py
+++ 14_Alt_solution_on_RE_sites.py
@@ -1,47 +1,27 @@
-"""Finding a shared motif between multiple strings"""
+# Solution from https://github.com/jssssv007/stackexcahnge/blob/master/thebillionyearwar.py
 
-# Adapted from https://www.geeksforgeeks.org/longest-common-substring-array-strings/?ref=rp
-
-
-with open("/Users/annatswater/Desktop/rosalind.revp.txt","r+") as f:
-
-    seq_array = []
-
-    for line in f:
-
-        if ">" not in line:
-
-            seq = []
-
-            if "\n" in line:
-
-                line = line.replace("\n", "")
-
-            seq_array.append(line) # create nested list for lists of nucleotides in each strin
-
-
-    common_motif = ""
+def reverse(l):
+	t=""
+	for i in range(len(l)):
+		if(l[i]=='A'):
+			t=t+'T'
+		elif(l[i]=='T'):
+			t=t+'A'
+		elif(l[i]=='C'):
+			t=t+'G'
+		elif(l[i]=='G'):
+			t=t+'C'
+	return t
     
-    # take the longest sequence as reference
+def rev(d):
+	return d[len(d)::-1]
 
-    longest_seq = max(seq_array)
-    longest_length = len(longest_seq)
+p=input()
 
-    for i in range(longest_length):
-
-        for j in range(i+1, longest_length+1):
-
-            # generate all possible substrings in the longest string
-
-            substring = longest_seq[i:j]
-
-            for k in range(1, len(seq_array)): # scan through all sequences
-
-                if substring not in seq_array[k]:
-                    break
-
-                elif (k+1 == len(seq_array) and len(common_motif) < len(substring)):
-                    common_motif = substring
-
-    print(common_motif)
-    
+for i in range(len(p)):
+	for j in range(4,14):
+		if (p[i:i+j]==rev(reverse(p[i:i+j]))and i+j<=len(p)):
+			print(i+1, end=" ")
+			print(j)
+# PS this worked
+# Why 
