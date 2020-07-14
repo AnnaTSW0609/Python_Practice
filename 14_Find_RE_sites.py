763ec0af63fc1189d5af68a2f24d28262e62e269
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-14 11:03:59 +0800

    Update 14_Find_RE_sites.py

--- 14_Find_RE_sites.py
+++ 14_Find_RE_sites.py
@@ -12,17 +12,14 @@ def ReverseComplement(string): # need to consider reverse complementary string a
     return(string.translate(trantab) [::-1]) # translate the string in the pattern defined in trantab, + reverse in our case
 
 RE_list = []
-RE_index_list = {}
 
 for i in range(len(DNA)):
 
-    for j in range(i+4, i+13):
+    for j in range(i+4, i+13): # index issue: if len = 12, i+13
 
         Substring = DNA[i:j]
 
         if len(Substring) >= 4:
-
-            print(Substring)
             
             rev_Substring = ReverseComplement(Substring)
             
@@ -36,8 +33,28 @@ for i in range(len(DNA)):
         
 print(RE_list)
 
+RE_index_list = [] # this list stores all the lists (some with one item) for the position matches
+
 for i in RE_list:
     Iter_index = [m.start() for m in re.finditer(i, DNA)] # this would return lists of position, one list for each of the RE substrings
+    RE_index_list.append(Iter_index)
+# So, the index of the item in RE_index_list will match the index of the items in RE_list
+# i.e. each palindrome in RE_list matches the positions in RE_index_list
+
+print(RE_index_list)
+            
+for i,j in zip(RE_list, RE_index_list):
+
+    if RE_list.index(i) == RE_index_list.index(j): # i.e. each palindrome corresponds to position of each list in the index list
+
+        for item in j: # return all matching positions in the list for each palindrome
+
+            print(item+1, len(i)) # +1 to match 1-based position
+            
+        
+        
+            
+            
     
     
     
