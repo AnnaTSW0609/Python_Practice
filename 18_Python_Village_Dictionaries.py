7fa34c0e451c8cde71ede6ddcaee9d675e743d76
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-17 21:24:43 +0800

    Update 18_Python_Village_Dictionaries.py

--- 18_Python_Village_Dictionaries.py
+++ 18_Python_Village_Dictionaries.py
@@ -1,3 +1,18 @@
 # https://stackoverflow.com/questions/9542738/python-find-in-list
 
 """count each word in a sentence"""
+
+sentence = input("Your string: ")
+
+sentence = sentence.split() # split the sentence into a list of words
+
+new_sentence=  list(dict.fromkeys(sentence)) # retain unique words as search items in a second list
+
+# https://www.w3schools.com/python/python_howto_remove_duplicates.asp
+
+for i in new_sentence:
+
+    print(i, sentence.count(i))
+    
+
+    
