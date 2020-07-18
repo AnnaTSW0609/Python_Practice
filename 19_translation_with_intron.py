0ce70b559af6e7c8be63cfb2791e633153cc19d6
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-18 12:07:14 +0800

    Update 19_translation_with_intron.py

--- 19_translation_with_intron.py
+++ 19_translation_with_intron.py
@@ -1,4 +1,18 @@
 """Translation with intron"""
 
 with open() as f:
+  
+  lst_seq = []
+  
   for line in f:
+    if ">" in line:
+      pass
+    else:
+      lst_seq.append(line)
+      
+  sequence = max(lst_seq)
+  lst_seq.pop(sequence)
+  
+  print(lst_seq)
+  print(sequence)
+      
