f29b627e72273b3bdbd2257e0db73f7dcca1567f
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-20 09:39:19 +0800

    Update 021_Finding_incontiguous_motif.py

--- 021_Finding_incontiguous_motif.py
+++ 021_Finding_incontiguous_motif.py
@@ -1,3 +1,13 @@
 """Finding motifs that would only exist after splicing"""
 """e.g. "ACG" is found in "TATGCTAAGATC""""
 """Pos 2: A, Pos 5: C, Pos 9: G (1-based)"""
+
+DNA = input("DNA string: ")
+
+motif = input("motif: ")
+
+motif = motif.split # spliting the motif characters into a list
+
+for char in motif:
+  
+  print(DNA.find(char))
