109b8291d8c7792b38d2ff85305fb0ebdf1a9181
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-20 10:01:53 +0800

    Update 021_Finding_incontiguous_motif.py

--- 021_Finding_incontiguous_motif.py
+++ 021_Finding_incontiguous_motif.py
@@ -1,13 +1,34 @@
 """Finding motifs that would only exist after splicing"""
-"""e.g. "ACG" is found in "TATGCTAAGATC""""
+"""e.g. "ACG" is found in "TATGCTAAGATC" """
 """Pos 2: A, Pos 5: C, Pos 9: G (1-based)"""
 
 DNA = input("DNA string: ")
 
 motif = input("motif: ")
 
-motif = motif.split # spliting the motif characters into a list
+motif = list(motif) # spliting the motif characters into a list
+
+ans = []
 
 for char in motif:
-  
-  print(DNA.find(char))
+
+    if len(ans) == 0: # if the list is empty
+      
+      pos = DNA.find(char)
+      
+      ans.append(pos+1) # 1 based index 
+      
+    else:
+      
+      pos = DNA.find(char, (ans[-1]+1))# the latest index in the list = the starting position for search for the next
+      
+      ans.append(pos+1)
+      
+print(*ans)
+
+# But this is very slow, how to improve on it?
+      
+      
+      
+      
+    
