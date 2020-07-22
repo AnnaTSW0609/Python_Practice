66e88cfe89842357cd3758ea4a74fd096254fefe
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-22 10:53:18 +0800

    Update 24_permutation_with_negative_numbers.py

--- 24_permutation_with_negative_numbers.py
+++ 24_permutation_with_negative_numbers.py
@@ -1,19 +1,11 @@
 """Permutation in this context: ordering of positive integers"""
-"""e.g. x = (5,4,3,2,1) is a permutation of length of 5"""
-"""Finding all possible permutations of n integers"""
+"""e.g. x = (-1, -2, -3, 1, 2, 3) is a permutation of length of 6"""
+"""Finding all possible permutations of n integers (if """
 
 n = int(input("Integer: "))
 
-Per = 1
+lst_no = [] # list of numbers
 
-for i in range(2, n+1): # return the total possible number of permutations, i.e. n!/(n-r)!, if select all (r=n), nPr = n!
+for x in range(1, n+1):
   
-  Per = Per*i
-  
-print(Per)
-
-from itertools import permutations
-
-for i in permutations(range(1,n+1), n): # print all permutations of range (1 to n) of length n
-    i = list(i)
-    print(*i)
+  lst_append(x)
