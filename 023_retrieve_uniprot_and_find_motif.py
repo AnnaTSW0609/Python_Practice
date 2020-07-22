067e5f088a88ca18aef212c826111a8e46009d58
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-22 10:42:29 +0800

    Update 023_retrieve_uniprot_and_find_motif.py
    This one didn't really worked, need to figure out how to really batch obtain sequence

--- 023_retrieve_uniprot_and_find_motif.py
+++ 023_retrieve_uniprot_and_find_motif.py
@@ -13,10 +13,19 @@ with open("/Users/annatswater/Desktop/uniprot_test.py", "r+") as f:
       all_id += " "
       all_id += line
 
+all_id = all_id.lstrip()
+
 print(all_id)
 
 # standard database identifier mapping from
 # https://www.uniprot.org/help/api_idmapping
+
+# https://stackoverflow.com/questions/35569042/ssl-certificate-verify-failed-with-python3
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
 import urllib.parse
 import urllib.request
 
@@ -26,7 +35,7 @@ params = {
 'from': 'ACC+ID',
 'to': 'ENSEMBL_ID',
 'format': 'tab',
-'query': all_id
+'query': "A2Z669 B5ZC00 P07204_TRBM_HUMAN P20840_SAG1_YEAST"
 }
 
 data = urllib.parse.urlencode(params)
@@ -37,3 +46,4 @@ with urllib.request.urlopen(req) as f:
 print(response.decode('utf-8'))
 
 # N-glycosylation motif is written as N{P}[ST]{P}.
+      
