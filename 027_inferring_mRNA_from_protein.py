3f109386486ede82a3883f94c60700f592c27c0e
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-24 09:25:47 +0800

    Update 027_inferring_mRNA_from_protein.py

--- 027_inferring_mRNA_from_protein.py
+++ 027_inferring_mRNA_from_protein.py
@@ -1,1 +1,15 @@
 """Inferring all possible mRNA that can give rise to a protein"""
+""" ans = total number of possible mRNA mod 1,000,000; to avoid storage of large numbers"""
+
+# Codon table
+
+bases = "ucag"
+codons = [a + b + c for a in bases for b in bases for c in bases] # random forming ttt, ttc,tta, ttg, to get the 64 codons
+amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'# already in the right order 
+codon_table = dict(zip(codons, amino_acids))
+
+# python modulo operator = %
+# 7 % 2 (7/2 = 3...1)
+# >>> 1
+
+
