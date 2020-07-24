2a67c07d60531feb824c4884fa12137aa7b6bc39
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-24 09:38:21 +0800

    Update 027_inferring_mRNA_from_protein.py

--- 027_inferring_mRNA_from_protein.py
+++ 027_inferring_mRNA_from_protein.py
@@ -3,10 +3,36 @@
 
 # Codon table
 
-bases = "ucag"
+bases = "UCAG"
 codons = [a + b + c for a in bases for b in bases for c in bases] # random forming ttt, ttc,tta, ttg, to get the 64 codons
 amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'# already in the right order 
 codon_table = dict(zip(codons, amino_acids))
+
+# for key, value in codon_table.items(): # item() yield key-value pairs
+    # print(key, "->", value)
+
+# Group keys with the same value by creating a new dictionary 
+# https://stackoverflow.com/questions/54249400/python-how-to-group-keys-that-have-the-same-values-in-a-dictionary
+
+# store the names (the keys of the new dict) as a set (keeps elements unique)
+names = set(codon_table.values())
+
+# use a list comprehension, iterating through keys and checking the values match each n
+# i.e. matching the amino acids in the names dictionary
+new_codon_table = {}
+for n in names:
+    new_codon_table[n] = [k for k in codon_table.keys() if codon_table[k] == n]
+    
+ # this will return 'I': ['AUU', 'AUC', 'AUA'], i.e. all possible codes for an amino acid
+
+for key, value in new_codon_table.items():
+  
+  value_count = len(value)
+  
+  new_codon_table[key] = len(value)
+
+print(new_codon_table)
+
 
 # python modulo operator = %
 # 7 % 2 (7/2 = 3...1)
