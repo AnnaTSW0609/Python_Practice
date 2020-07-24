d294876ff4df5e990fa4930867dcf8ff5cb6b127
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-24 10:00:05 +0800

    Update 027_inferring_mRNA_from_protein.py

--- 027_inferring_mRNA_from_protein.py
+++ 027_inferring_mRNA_from_protein.py
@@ -30,9 +30,30 @@ for key, value in new_codon_table.items():
   value_count = len(value)
   
   new_codon_table[key] = len(value)
-
+    
 print(new_codon_table)
 
+Prot = input("Your protein: ")
+
+Prot += "*" # add back the stop codon
+
+M = 1 # there is only 1 start codon
+
+for aa in Prot:
+    
+    aa_prob = int(new_codon_table.get(aa)) # get the total number of codons for an aa
+    
+    M = (M*aa_prob)%1000000
+
+print(M)
+    
+
+    
+        
+
+        
+        
+        
 
 # python modulo operator = %
 # 7 % 2 (7/2 = 3...1)
