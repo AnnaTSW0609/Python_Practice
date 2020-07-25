51243c349d0894f6eb04c5d27929a0a88863b503
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-25 11:52:50 +0800

    Update 023_retrieve_uniprot_and_find_motif.py
    Two errors here:
    
    1. Should have used url lib and sys for accessing the fast file using "http://www.uniprot.org/uniprot/%s.fasta"
    
    2. Should have used r'(?=N[^P][ST][^P])', to avoid missing overlapps

--- 023_retrieve_uniprot_and_find_motif.py
+++ 023_retrieve_uniprot_and_find_motif.py
@@ -7,7 +7,7 @@
 
 all_id = [] 
 
-with open("/Users/annatswater/Desktop/uniprot_test.py", "r+") as f:
+with open("/Users/annatswater/Desktop/rosalind_mprt.txt", "r+") as f:
    
    for line in f:
       
@@ -50,10 +50,39 @@ for item, accession in zip(records,all_id):
 # For example, the N-glycosylation motif is written as N{P}[ST]{P}.
 # so it's N-(not P)-(either S or T)-(not P)
 
-import re # import the re module for parsing 
+import re # import the re module for parsing
+import regex # https://stackoverflow.com/questions/34774126/re-finditer-returning-same-value-for-start-and-end-methods
 motif = r"N[^P](S|T)[^P]" # https://pythonforbiologists.com/regular-expressions
 
+position = {} # a dictionary to store the accession number and the list of matches (indices)
+index = 0 # initialize starting position, so that wouldn't miss those closely together 
+
+# Method from https://www.cnblogs.com/think-and-do/p/7283840.html
+# Regex version of method from https://www.geeksforgeeks.org/python-count-overlapping-substring-in-a-given-string/
+
+
+# This one from 
+# https://stackoverflow.com/questions/2674391/python-locating-the-position-of-a-regex-match-in-a-string
+# to return multiple matches and their positions
+# but will miss overlaps
+
+# Sol 1: use regex, which has overlapped parameter in finditer
+
+# Sol 2: http://rosalind.info/problems/mprt/solutions/
+# Hascool actually does have it right,
+# because he uses '(?=N[^P][ST][^P])',
+# which matches all '' (no character) that is followed by the motif,
+# without actually consuming the motif. Overlapping matches will be found.
+
 for item in ID_and_seq:
-   pos = [] # collection of all positions
-   pos.append(re.search(motif, ID_and_seq[item]).start())
-   
+   # these two lines search for multiple matches 
+   iter = regex.finditer(motif, ID_and_seq[item], overlapped=1)
+   indices = [m.start()+1 for m in iter]
+   # this line saves the list of matches to the dict
+   position [item] = indices
+
+# to return the answer in Rosalind format
+for entry in position:
+   if position[entry] != []:
+      print(entry)
+      print(*position[entry])
