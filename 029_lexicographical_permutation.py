39713895bb4627ed1b26275968b6dd566d5276dc
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-26 10:07:47 +0800

    Update 029_lexicographical_permutation.py

--- 029_lexicographical_permutation.py
+++ 029_lexicographical_permutation.py
@@ -1,2 +1,16 @@
 """given a list of characters, and the length of permutation required"""
 """return all the permutations in lexigraphical order"""
+
+with open ("filename","r+") as f:
+  
+  for line in f:
+    
+    if len(line)>1:
+      line = sorted(line.split(" "))
+    
+    else:
+      number = line
+
+print(line)
+print(number)
+      
