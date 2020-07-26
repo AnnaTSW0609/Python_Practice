e5cdef59bcc386830d9430e19b018cd8541a90c0
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-26 09:40:49 +0800

    Update 028_Partial_Permutation.py

--- 028_Partial_Permutation.py
+++ 028_Partial_Permutation.py
@@ -4,14 +4,20 @@ n = int(input("n: "))
 
 k = int(input("k: "))
 
-def permutation(x):
+def perm(x):
   
   Per = 1
 
   for i in range(2, x+1): # return the total possible number of permutations, i.e. n!/(n-r)!, if select all (r=n), nPr = n!
   
-    Per = Per*i
+    Per *= i
+
+    # Per %= 10**6 # only doing the modulo at the end doesn't prevent the overflow, or does it?
   
   return Per 
 
-print((int(permutation(n))/int(permutation(n-k)))%1000000)
+print((perm(n)/perm(n-k))%10**6)
+
+
+
+
