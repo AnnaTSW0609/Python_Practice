a7a5e970886e1c7c8b25ac032ccb4e993397b526
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-26 09:30:50 +0800

    Update 028_Partial_Permutation.py

--- 028_Partial_Permutation.py
+++ 028_Partial_Permutation.py
@@ -1,1 +1,17 @@
-"""permutation of k out of n numbers"""
+"""permutation of k out of n numbers, mod 1,000,000"""
+
+n = int(input("n: "))
+
+k = int(input("k: "))
+
+def permutation(x):
+  
+  Per = 1
+
+  for i in range(2, x+1): # return the total possible number of permutations, i.e. n!/(n-r)!, if select all (r=n), nPr = n!
+  
+    Per = Per*i
+  
+  return Per 
+
+print((int(permutation(n))/int(permutation(n-k)))%1000000)
