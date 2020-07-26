043f081baedd2eaec6d54536476a95afa1da71c3
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-26 10:08:01 +0800

    Update 029_lexicographical_permutation.py

--- 029_lexicographical_permutation.py
+++ 029_lexicographical_permutation.py
@@ -1,16 +1,32 @@
 """given a list of characters, and the length of permutation required"""
 """return all the permutations in lexigraphical order"""
 
-with open ("filename","r+") as f:
+with open ("/Users/annatswater/Desktop/rosalind_pper.txt","r+") as f:
   
   for line in f:
     
     if len(line)>1:
-      line = sorted(line.split(" "))
+      char_lst = sorted(line.strip().split(" "))
     
     else:
-      number = line
+      number = int(line)
 
-print(line)
-print(number)
-      
+def lexi_perm(char, num): # char =  list of characters
+
+    from itertools import product # native python function, not permutation but Cartesian product
+
+    # https://stackoverflow.com/questions/3099987/generating-permutations-with-repetitions
+    
+    for i in list(product(char,repeat = num)): # permutations (list to permutate, length)
+
+        i = list(i) # returns tuple, so turn into list first
+
+        output = ""
+
+        for x in i:
+
+            output += x # required format: string
+
+        print(output)
+
+lexi_perm(char_lst,number)
