2016423af5bb134a73a2321b91aa933180c341a7
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-27 09:53:45 +0800

    Update 030_Ordering_strings_lex_different_lengths.py

--- 030_Ordering_strings_lex_different_lengths.py
+++ 030_Ordering_strings_lex_different_lengths.py
@@ -1,1 +1,16 @@
 """Ordering different strings of different lengths lexicographically"""
+
+with open("/Users/annatswater/Desktop/rosalind_pper.txt", "r+") as f:
+  
+  for line in f:
+    
+    if line.isdigit() == True:
+      
+      length = line
+    
+    else:
+      
+      alphabet = sorted(line.strip().split(" "))
+
+print(length)
+print(alphabet)
