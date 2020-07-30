0827bb73be464111a10cbe8c32ad9edd828a34a6
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-30 15:09:17 +0800

    Update 033_Matching_Random_Motif.py

--- 033_Matching_Random_Motif.py
+++ 033_Matching_Random_Motif.py
@@ -1,3 +1,19 @@
 """Given an integer N, GC% x and a random DNA string s"""
 """Return probability if N random strings is constructed with GC% x, at least 1 = s"""
 
+with open(file, "r+") as f:
+  
+  for line in f:
+    
+    if line.isalpha() == True:
+      
+      s = line.strip()
+      
+    else:
+      
+      lst = line.strip().split()
+      
+      N = lst [0]
+      
+      x = lst [1]
+
