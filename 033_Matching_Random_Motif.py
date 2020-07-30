38de70db420a810b32ff82d686e2af98990a1cc4
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-30 15:24:02 +0800

    Update 033_Matching_Random_Motif.py

--- 033_Matching_Random_Motif.py
+++ 033_Matching_Random_Motif.py
@@ -1,19 +1,19 @@
 """Given an integer N, GC% x and a random DNA string s"""
 """Return probability if N random strings is constructed with GC% x, at least 1 = s"""
 
-with open(file, "r+") as f:
+with open("/Users/annatswater/Desktop/rosalind_rstr.txt", "r+") as f:
   
   for line in f:
     
     if line.isalpha() == True:
       
       s = line.strip()
-      
+
     else:
       
       lst = line.strip().split()
       
-      N = lst [0]
+      N = float(lst [0]) # float**float =  True
       
       x = lst [1]
 
@@ -35,4 +35,7 @@ for base in s:
       
       Prob *= (float(x)/2) 
 
-print(Prob)      
+# Compute 1- not P in N sequence, i.e. 1-P**N
+# Ref: https://www.youtube.com/watch?v=dwjQaJ5xt1o
+
+print("{:.3f}".format(1- (1-Prob)**N))
