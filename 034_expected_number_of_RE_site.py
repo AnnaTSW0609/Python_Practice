bfa18b2194b0eb280ab4af8530b711f354b37fe5
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-07-31 10:37:39 +0800

    Create 034_expected_number_of_RE_site.py

---
+++ 034_expected_number_of_RE_site.py
@@ -0,0 +1,3 @@
+"""Given: number N, DNA string s, Array A"""
+"""Return: Array B containing expected number of times that s will be a substring of t"""
+"""t = strings length N and with GC% of A[i]"""
